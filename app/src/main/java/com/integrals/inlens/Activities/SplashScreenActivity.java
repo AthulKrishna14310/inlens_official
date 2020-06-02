dfc3371271d78ede2b435b8c35cd7a7ff377be2b
Elson Jose <elsonjoseofficial@gmail.com>
2020-06-02 12:26:50 +0530

    HandleQuit Asynctask
    1. to prevent freezing or lagging in screen

--- app/src/main/java/com/integrals/inlens/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/integrals/inlens/Activities/SplashScreenActivity.java
@@ -1,39 +1,62 @@
 package com.integrals.inlens.Activities;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.integrals.inlens.AsynchTasks.HandleQuit;
 import com.integrals.inlens.Helper.AppConstants;
 import com.integrals.inlens.Helper.FirebaseConstants;
+import com.integrals.inlens.Helper.QRCodeBottomSheet;
 import com.integrals.inlens.Helper.ReadFirebaseData;
 import com.integrals.inlens.Interface.FirebaseRead;
 import com.integrals.inlens.MainActivity;
 import com.integrals.inlens.R;
+import com.integrals.inlens.WorkManager.AlbumScanWorker;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static com.integrals.inlens.Helper.AppConstants.MY_PERMISSIONS_REQUEST_START_WORKMANAGER;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
-    DatabaseReference userRef;
+    DatabaseReference userRef,communityRef,linkRef;
     List<String> userCommunityIdList;
-    String currentUserId;
+    String currentUserId,currentActiveCommunityID=AppConstants.NOT_AVALABLE;
     static final int DELAY_IN_MILLIS=1000;
-    ValueEventListener listener;
+    ValueEventListener listener,communityRefListenerForActiveAlbum;
 
     String appTheme="";
 
@@ -73,6 +96,9 @@ public class SplashScreenActivity extends AppCompatActivity {
         userCommunityIdList = new ArrayList<>();
         firebaseAuth = FirebaseAuth.getInstance();
         userRef = FirebaseDatabase.getInstance().getReference().child(FirebaseConstants.USERS);
+        communityRef = FirebaseDatabase.getInstance().getReference().child(FirebaseConstants.COMMUNITIES);
+        linkRef = FirebaseDatabase.getInstance().getReference().child(FirebaseConstants.INVITE_LINK);
+
         CheckUserAuthentication();
 
 
@@ -92,6 +118,64 @@ public class SplashScreenActivity extends AppCompatActivity {
                         for (DataSnapshot snapshot : datasnapshot.child(FirebaseConstants.COMMUNITIES).getChildren()) {
                             userCommunityIdList.add(snapshot.getKey());
                         }
+                    }
+                    SharedPreferences LastShownNotificationInfo = getSharedPreferences(AppConstants.CURRENT_COMMUNITY_PREF, Context.MODE_PRIVATE);
+                    if (datasnapshot.hasChild(FirebaseConstants.LIVECOMMUNITYID)) {
+
+                        currentActiveCommunityID = datasnapshot.child(FirebaseConstants.LIVECOMMUNITYID).getValue().toString();
+                        if (!LastShownNotificationInfo.contains("id")) {
+                            SharedPreferences.Editor editor = LastShownNotificationInfo.edit();
+                            editor.putString("id", currentActiveCommunityID);
+                            editor.commit();
+                        }
+
+                        communityRefListenerForActiveAlbum = readFirebaseData.readData(communityRef.child(currentActiveCommunityID), new FirebaseRead() {
+                            @Override
+                            public void onSuccess(DataSnapshot snapshot) {
+
+                                // optimization 1 resulted in this error, everytime the album is quit even if the album is inactive
+                                // so first check the album  has status status;
+                                if (snapshot.hasChild(FirebaseConstants.COMMUNITYSTATUS)) {
+
+
+                                    String status = snapshot.child(FirebaseConstants.COMMUNITYSTATUS).getValue().toString();
+                                    if (status.equals("T")) {
+                                        long endtime = Long.parseLong(snapshot.child(FirebaseConstants.COMMUNITYENDTIME).getValue().toString());
+
+                                        if (!LastShownNotificationInfo.contains("stopAt")) {
+                                            SharedPreferences.Editor editor = LastShownNotificationInfo.edit();
+                                            editor.putString("stopAt", String.valueOf(endtime));
+                                            editor.commit();
+                                        }
+
+                                        TimeZone timeZone = TimeZone.getDefault();
+                                        long offsetInMillis = timeZone.getOffset(Calendar.ZONE_OFFSET);
+                                        long serverTimeInMillis = (System.currentTimeMillis() - offsetInMillis);
+                                        //Log.i("timeQuit", "Server : " + serverTimeInMillis + " End : " + endtime + " Systemmillis : " + System.currentTimeMillis());
+                                        if (serverTimeInMillis >= endtime) {
+                                            quitCloudAlbum();
+
+                                        }
+                                    } else {
+                                        // stop the necessary services
+                                        WorkManager.getInstance().cancelAllWorkByTag(AppConstants.PHOTO_SCAN_WORK);
+
+                                    }
+                                }
+
+                            }
+
+                            @Override
+                            public void onStart() {
+
+                            }
+
+                            @Override
+                            public void onFailure(DatabaseError databaseError) {
+
+
+                            }
+                        });
                     }
                     new Handler().postDelayed(new Runnable() {
                         @Override
@@ -130,6 +214,15 @@ public class SplashScreenActivity extends AppCompatActivity {
 
 
         }
+    }
+
+
+    private void quitCloudAlbum() {
+
+        HandleQuit handleQuit = new HandleQuit(getApplicationContext(),userRef.child(currentUserId),linkRef,communityRef.child(currentActiveCommunityID).child(FirebaseConstants.COMMUNITYSTATUS),currentActiveCommunityID);
+        handleQuit.execute();
+
+
     }
 
     @Override
---
+++ app/src/main/java/com/integrals/inlens/AsynchTasks/HandleQuit.java
@@ -0,0 +1,112 @@
+package com.integrals.inlens.AsynchTasks;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import androidx.work.WorkManager;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.integrals.inlens.Helper.AppConstants;
+import com.integrals.inlens.Helper.FirebaseConstants;
+
+import java.util.UUID;
+
+public class HandleQuit extends AsyncTask<Void,Void,Void>
+{
+
+    private DatabaseReference linkRef,statusRef,currentUserRef;
+    private String activeCommunityId;
+    private boolean isTaskSuccessful;
+    private Context context;
+    SharedPreferences CurrentActiveCommunity;
+
+    public HandleQuit(Context applicationContext, DatabaseReference currentUserRef, DatabaseReference linkRef, DatabaseReference statusRef, String currentActiveCommunityID) {
+
+        context=applicationContext;
+        this.currentUserRef= currentUserRef;
+        this.linkRef=linkRef;
+        this.statusRef=statusRef;
+        activeCommunityId=currentActiveCommunityID;
+        isTaskSuccessful=false;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        CurrentActiveCommunity = context.getSharedPreferences(AppConstants.CURRENT_COMMUNITY_PREF, Context.MODE_PRIVATE);
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        linkRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot snapshot:dataSnapshot.getChildren())
+                {
+                    if(snapshot.hasChild("id") && snapshot.child("id").equals(activeCommunityId))
+                    {
+                        linkRef.child(snapshot.getKey()).removeValue();
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        statusRef.removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+
+                    currentUserRef.child(FirebaseConstants.LIVECOMMUNITYID).removeValue();
+                    String scanWorkId = CurrentActiveCommunity.getString("scanWorkerId", AppConstants.NOT_AVALABLE);
+                    String albumEndWorkId = CurrentActiveCommunity.getString("albumendWorkerId", AppConstants.NOT_AVALABLE);
+                    if (scanWorkId.equals(AppConstants.NOT_AVALABLE)) {
+                        WorkManager.getInstance().cancelUniqueWork(AppConstants.PHOTO_SCAN_WORK);
+                    } else {
+                        WorkManager.getInstance().cancelWorkById(UUID.fromString(scanWorkId));
+                    }
+                    if (albumEndWorkId.equals(AppConstants.NOT_AVALABLE)) {
+                        WorkManager.getInstance().cancelAllWork();
+                    } else {
+                        WorkManager.getInstance().cancelWorkById(UUID.fromString(albumEndWorkId));
+                    }
+
+                    isTaskSuccessful=true;
+
+                }
+            }
+        });
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        if(isTaskSuccessful)
+        {
+            Toast.makeText(context, "Successfully left the album.", Toast.LENGTH_SHORT).show();
+
+        }
+        else
+        {
+            Toast.makeText(context, "Album quit unsuccessful.", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+}
