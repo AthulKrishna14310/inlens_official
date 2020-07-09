2b7677f5994e0997ee6a1e2b0fac1a2f349a746d
AthulKrishna14310 <athul.krishna14310@gmail.com>
2020-07-09 19:23:09 +0530

    fix for test

--- app/src/main/java/com/integrals/inlens/Helper/SnackShow.java
+++ app/src/main/java/com/integrals/inlens/Helper/SnackShow.java
@@ -3,36 +3,36 @@ package com.integrals.inlens.Helper;
 import android.app.Activity;
 import android.view.View;
 
-import com.ankushyerwar.floatingsnackbar.SnackBar;
-import com.google.android.material.snackbar.Snackbar;
-import com.integrals.inlens.R;
+import de.mateware.snacky.Snacky;
 
 
 public class SnackShow {
     private View view;
     private Activity context;
-    private Snackbar snack;
-
-    public Snackbar getSnack() {
-        return snack;
-    }
-
-    public void setSnack(Snackbar snack) {
-        this.snack = snack;
-    }
 
     public SnackShow(View view, Activity context){
         this.view=view;
         this.context=context;
     }
     public void showErrorSnack(String message){
-        SnackBar.error(view, message, SnackBar.LENGTH_LONG).show();
+        Snacky.builder()
+                .setActivity(context)
+                .error().setText(message)
+                .show();
     }
     public void showSuccessSnack(String message){
-        SnackBar.success(view, message, SnackBar.LENGTH_LONG).show();
+        Snacky.builder()
+                .setActivity(context)
+                .setMaxLines(1)
+                .success().setText(message)
+                .show();
 
     }
     public void showInfoSnack(String message){
-        SnackBar.info(view,message, SnackBar.LENGTH_LONG).show();
+        Snacky.builder()
+                .setActivity(context)
+                .info().setText(message)
+                .show();
+
     }
 }
