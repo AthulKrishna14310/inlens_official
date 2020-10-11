07513ec9a0cb7695b38e5836ab2755b40e07e4e4
elsonjose <elsonjoseofficial@gmail.com>
2020-10-11 15:45:08 +0530

    Enhancement : EventType Buttons Updated with Chips
    The eventtype selection which was initially performed using 2 buttons
    aligned hoziontally with 3 rows, giving 3 rows and 6 buttons is now
    replaced with a chipgroup. The chipgroup layout prvide the following
    features
    1. Ability to add custom tags
    2. Ability to remove custom tags (default tags are persistent)
    3. Ability to add multiple tags for an event.
    
    This in turn results makes changes in event type layout in bottomsheets
    1. Bottomsheet_menu_item_active
    2. Bottomsheet_menu_item_inactive
    
    New issues will to created in reference to this update

--- app/src/main/java/com/integrals/inlens/Database/UploadQueueDB.java
+++ app/src/main/java/com/integrals/inlens/Database/UploadQueueDB.java
@@ -13,19 +13,19 @@ import com.integrals.inlens.Helper.AppConstants;
 public class UploadQueueDB extends SQLiteOpenHelper {
 
     public UploadQueueDB(@Nullable Context context) {
-        super(context, AppConstants.SQLITE_DB_NAME, null, 1);
+        super(context, AppConstants.UPLOAD_SQLITE_DB_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
-        sqLiteDatabase.execSQL("create table "+AppConstants.SQLITE_TABLE_NAME+" ("+AppConstants.SQLITE_COLUMN_FIELD_ID+" TEXT PRIMARY KEY ,"+AppConstants.SQLITE_COLUMN_FIELD_URI+" TEXT, "+AppConstants.SQLITE_COLUMN_FIELD_QUEUED_TIME +" TEXT )");
+        sqLiteDatabase.execSQL("create table "+AppConstants.UPLOAD_SQLITE_TABLE_NAME +" ("+AppConstants.UPLOAD_SQLITE_COLUMN_FIELD_ID +" TEXT PRIMARY KEY ,"+AppConstants.UPLOAD_SQLITE_COLUMN_FIELD_URI +" TEXT, "+AppConstants.UPLOAD_SQLITE_COLUMN_FIELD_QUEUED_TIME +" TEXT )");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
-        sqLiteDatabase.execSQL("drop table if exists "+AppConstants.SQLITE_TABLE_NAME);
+        sqLiteDatabase.execSQL("drop table if exists "+AppConstants.UPLOAD_SQLITE_TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 
@@ -33,30 +33,30 @@ public class UploadQueueDB extends SQLiteOpenHelper {
     {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(AppConstants.SQLITE_COLUMN_FIELD_ID,name);
-        contentValues.put(AppConstants.SQLITE_COLUMN_FIELD_URI,uri);
-        contentValues.put(AppConstants.SQLITE_COLUMN_FIELD_QUEUED_TIME,time);
-        long result = db.insert(AppConstants.SQLITE_TABLE_NAME,null,contentValues);
+        contentValues.put(AppConstants.UPLOAD_SQLITE_COLUMN_FIELD_ID,name);
+        contentValues.put(AppConstants.UPLOAD_SQLITE_COLUMN_FIELD_URI,uri);
+        contentValues.put(AppConstants.UPLOAD_SQLITE_COLUMN_FIELD_QUEUED_TIME,time);
+        long result = db.insert(AppConstants.UPLOAD_SQLITE_TABLE_NAME,null,contentValues);
         return result != -1;
     }
 
     public Cursor getQueuedData()
     {
         SQLiteDatabase db = this.getWritableDatabase();
-        return db.rawQuery("select * from "+ AppConstants.SQLITE_TABLE_NAME,null);
+        return db.rawQuery("select * from "+ AppConstants.UPLOAD_SQLITE_TABLE_NAME,null);
     }
 
     public boolean deleteData(String name)
     {
         SQLiteDatabase db = this.getWritableDatabase();
-        long result = db.delete(AppConstants.SQLITE_TABLE_NAME,AppConstants.SQLITE_COLUMN_FIELD_ID+"=?",new String[]{name});
+        long result = db.delete(AppConstants.UPLOAD_SQLITE_TABLE_NAME,AppConstants.UPLOAD_SQLITE_COLUMN_FIELD_ID +"=?",new String[]{name});
         return result != 0;
     }
 
     public void deleteAllData()
     {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.execSQL("delete from "+AppConstants.SQLITE_TABLE_NAME);
+        db.execSQL("delete from "+AppConstants.UPLOAD_SQLITE_TABLE_NAME);
     }
     
 }
